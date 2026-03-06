c927f7b678f0ab38c06844afb210c2845c986630
vinip1250-art <vinip1250@gmail.com>
2026-03-06 06:53:47 -0300

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import { catalogCache } from "./cache.js";
+
+app.get("/catalog/:type/:id.json", async (req, res) => {
+
+  const cacheKey = req.originalUrl;
+
+  const cached = catalogCache.get(cacheKey);
+  if (cached) {
+    return res.json(cached);
+  }
+
+  const result = await buildCatalog();
+
+  catalogCache.set(cacheKey, result);
+
+  res.json(result);
+
+});
